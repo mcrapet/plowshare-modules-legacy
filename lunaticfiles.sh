bb682cf3c9f55b9932b5107d4bf6140c942a2df5
Raziel-23 <venom23@runbox.com>
2016-02-11 17:27:22 +0100

    lunaticfiles (probe): file sizes are actually in 'iB', not in 'B'

--- lunaticfiles.sh
+++ lunaticfiles.sh
@@ -191,8 +191,9 @@ lunaticfiles_probe() {
     fi
 
     if [[ $REQ_IN = *s* ]]; then
-        FILE_SIZE=$(parse '<[Pp][[:space:]].*\[' '\[\(.*\)\]' <<< "$PAGE") && \
-            translate_size "$FILE_SIZE" && REQ_OUT="${REQ_OUT}s"
+        FILE_SIZE=$(parse '<[Pp][[:space:]].*\[' '\[\(.*\)\]' <<< "$PAGE") \
+            && FILE_SIZE=$(replace 'B' 'iB' <<< $FILE_SIZE) \
+            && translate_size "$FILE_SIZE" && REQ_OUT="${REQ_OUT}s"
     fi
 
     if [[ $REQ_IN = *i* ]]; then
