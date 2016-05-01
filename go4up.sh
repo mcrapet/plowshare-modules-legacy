c126950577540d4b4b537ec2770f1d67124ba58f
Raziel-23 <venom23@runbox.com>
2016-05-01 11:08:13 +0200

    go4up: massive refactoring
    All options were preserved and now working properly. Only one convenient
    change: --include accepts host names instead of its IDs, example:
    --include '1fichier, openload'.
    
    Fixes #11.

--- go4up.sh
+++ go4up.sh
@@ -1,5 +1,5 @@
 # Plowshare go4up.com module
-# Copyright (c) 2012-2015 Plowshare team
+# Copyright (c) 2012-2016 Plowshare team
 #
 # This file is part of Plowshare.
 #
@@ -16,11 +16,11 @@
 # You should have received a copy of the GNU General Public License
 # along with Plowshare.  If not, see <http://www.gnu.org/licenses/>.
 
-MODULE_GO4UP_REGEXP_URL='http://\(www\.\)\?go4up\.com'
+MODULE_GO4UP_REGEXP_URL='https\?://\(www\.\)\?go4up\.com'
 
 MODULE_GO4UP_UPLOAD_OPTIONS="
 AUTH_FREE,b,auth-free,a=EMAIL:PASSWORD,Free account
-INCLUDE,,include,l=LIST,Provide list of host site (comma separated)
+INCLUDE,,include,l=LIST,Provide list of host names (comma separated)
 COUNT,,count,n=COUNT,Take COUNT mirrors (hosters) from the available list. Default is 5.
 API,,api,,Use public API (recommended)"
 MODULE_GO4UP_UPLOAD_REMOTE_SUPPORT=yes
@@ -31,6 +31,13 @@ AUTH_FREE,b,auth-free,a=EMAIL:PASSWORD,Free account (mandatory)"
 MODULE_GO4UP_LIST_OPTIONS=""
 MODULE_GO4UP_LIST_HAS_SUBFOLDERS=no
 
+# Switch language to english
+# $1: cookie file
+# $2: base URL
+go4up_switch_lang() {
+    curl "$2/home/lang/en" -c "$1" > /dev/null || return
+}
+
 # Static function. Proceed with login
 # $1: authentication
 # $2: cookie file
@@ -39,24 +46,38 @@ go4up_login() {
     local AUTH_FREE=$1
     local COOKIE_FILE=$2
     local BASE_URL=$3
-    local LOGIN_DATA PAGE NAME
+    local CV PAGE MSG LOGIN_DATA JSON STATUS NAME
 
-    LOGIN_DATA='email=$USER&password=$PASSWORD&signin_go='
-    PAGE=$(post_login "$AUTH_FREE" "$COOKIE_FILE" "$LOGIN_DATA" \
-       "$BASE_URL/login.php" -b "$COOKIE_FILE") || return
+    if CV=$(storage_get 'cookie_file'); then
+        echo "$CV" >"$COOKIE_FILE"
 
-    if match 'Bad email/password.' "$PAGE" || \
-        match 'Please enter a valid email address' "$PAGE"; then
-        return $ERR_LOGIN_FAILED
+        # Check for expired session.
+        PAGE=$(curl -b "$COOKIE_FILE" "$BASE_URL/account") || return
+        if ! match 'id="oldEmail"' "$PAGE"; then
+            storage_set 'cookie_file'
+            return $ERR_EXPIRED_SESSION
+        fi
+
+        log_debug 'session (cached)'
+        MSG='reused login for'
+    else
+        LOGIN_DATA='email=$USER&password=$PASSWORD&remember-me=remember-me'
+        JSON=$(post_login "$AUTH_FREE" "$COOKIE_FILE" "$LOGIN_DATA" \
+            "$BASE_URL/login/process" -b "$COOKIE_FILE") || return
+
+        # If successful we get JSON: {"status":1,"redirect":"home","msg":"Success !"}
+        STATUS=$(parse_json 'status' <<< "$JSON") || return
+        [ "$STATUS" != '1' ] && return $ERR_LOGIN_FAILED
+
+        storage_set 'cookie_file' "$(cat "$COOKIE_FILE")"
+
+        PAGE=$(curl -b "$COOKIE_FILE" "$BASE_URL/account") || return
+        log_debug 'session (new)'
+        MSG='logged in as'
     fi
 
-    PAGE=$(curl -b "$COOKIE_FILE" "$BASE_URL/account.php")
-
-    # The new password will be confirmed at : <b>USER_MAIL</b>
-    NAME=$(echo "$PAGE" | parse_tag \
-        'The new password will be confirmed at' b) || return
-
-    log_debug "Successfully logged in as member '$NAME'"
+    NAME=$(parse_attr_quiet 'id="oldEmail"' 'value' <<< "$PAGE")
+    log_debug "Successfully $MSG 'free' member '$NAME'"
 }
 
 # Upload a file to go4up.com
@@ -69,9 +90,9 @@ go4up_upload() {
     local FILE=$2
     local DESTFILE=$3
     local BASE_URL='http://go4up.com'
-    local PAGE LINK FORM UPLOAD_ID USER_ID UPLOAD_BASE_URL
-    local SITE HOST SITES_ALL SITES_SEL SITES_FORM SITES_MULTI
+    local UPLOAD_URL PAGE ERR USER PASSWORD
 
+    # Upload by using public API: http://go4up.com/misc/apidoc
     if [ -n "$API" ]; then
         log_debug 'using public API'
 
@@ -86,174 +107,182 @@ go4up_upload() {
             return $ERR_BAD_COMMAND_LINE
         fi
 
-        if match_remote_url "$FILE"; then
-            log_error 'Public API does not support remote upload.'
-            return $ERR_BAD_COMMAND_LINE
-        fi
-
-        local USER PASSWORD
         split_auth "$AUTH_FREE" USER PASSWORD || return
 
-        # http://go4up.com/wiki/index.php/API_doc
-        PAGE=$(curl -F "user=$USER" -F "pass=$PASSWORD" \
-            -F "filedata=@$FILE" "$BASE_URL/api/upload.php")
+        UPLOAD_URL=$(curl "$BASE_URL/api/getserver") || return
 
-        if match '<link>' "$PAGE"; then
-            : # Nothing to do, just catch the "good" case
-        elif match '>Invalid login/password<' "$PAGE"; then
-            return $ERR_LOGIN_FAILED
-        # Invalid post data count
-        # Choose host to upload in your account
+        # Upload local file
+        if ! match_remote_url "$FILE"; then
+            PAGE=$(curl_with_log -F "user=$USER" -F "pass=$PASSWORD" \
+                -F "filedata=@$FILE" "$UPLOAD_URL") || return
+
+        # Upload remote file
         else
-            local ERR=$(echo "$PAGE" | parse_tag error)
+            PAGE=$(curl_with_log -F "user=$USER" -F "pass=$PASSWORD" \
+                --form-string "url=$FILE" "$UPLOAD_URL") || return
+        fi
+
+        if match '<error>' "$PAGE"; then
+            ERR=$(parse_tag 'error' <<< "$PAGE")
             log_error "Remote error: $ERR"
             return $ERR_FATAL
         fi
 
-        echo "$PAGE" | parse_tag 'link'
+        parse_tag 'link' <<< "$PAGE" || return
         return 0
     fi
 
-    # Public API not used
+    # Upload by using WWW.
+    local FORM UPLOAD_ID USER_ID HOSTS_NAMES HOSTS_NUMS
+    local NAME NUM HOSTS_SEL HOST_FOUND FORM_HOSTS
+
+    # Note: This is out first contact with Go4Up, so we need -c in curl command.
+    go4up_switch_lang "$COOKIE_FILE" "$BASE_URL"
 
     # Login needs to go before retrieving hosters because accounts
-    # have a individual hoster lists
+    # have a individual host list
     if [ -n "$AUTH_FREE" ]; then
         go4up_login "$AUTH_FREE" "$COOKIE_FILE" "$BASE_URL" || return
     fi
 
-    # Retrieve complete hosting site list
-    # Note: This can be either our first contact with Go4Up (no login)
-    #       or the second (with login), so we need both -b and -c.
-    if match_remote_url "$FILE"; then
-        PAGE=$(curl -b "$COOKIE_FILE" -c "$COOKIE_FILE" \
-            "$BASE_URL/remote.php") || return
-        FORM=$(grep_form_by_id "$PAGE" 'form_upload' | break_html_lines) || return
-        UPLOAD_BASE_URL=$(echo "$FORM" | parse_form_action) || return
+    # Retrieve form with data and with complete hosting list
+    if ! match_remote_url "$FILE"; then
+        PAGE=$(curl -b "$COOKIE_FILE" "$BASE_URL") || return
+        FORM=$(grep_form_by_id "$PAGE" 'myformupload' | break_html_lines) || return
     else
-        PAGE=$(curl -b "$COOKIE_FILE" -c "$COOKIE_FILE" \
-            "$BASE_URL") || return
-        UPLOAD_BASE_URL=$(echo "$PAGE" | parse_attr iframe src) || return
-        PAGE=$(curl "$UPLOAD_BASE_URL") || return
-        FORM=$(grep_form_by_id "$PAGE" 'ubr_upload_form' | break_html_lines) || return
+        PAGE=$(curl -b "$COOKIE_FILE" "$BASE_URL/home/remote") || return
+        FORM=$(grep_form_by_id "$PAGE" 'form_upload' | break_html_lines) || return
     fi
 
-    UPLOAD_BASE_URL=$(basename_url "$UPLOAD_BASE_URL") || return
-    log_debug "Upload base URL: $UPLOAD_BASE_URL"
+    UPLOAD_URL=$(parse_attr 'action' <<< "$FORM") || return
+    log_debug "Upload base URL: $UPLOAD_URL"
 
-    # When logged into account all form fields are on a single line
-    if [ -n "$AUTH_FREE" ]; then
-        FORM=$(echo "$FORM" | break_html_lines)
-    fi
+    UPLOAD_ID=$(parse_form_input_by_name 'uploadID' <<< "$FORM") || return
+    USER_ID=$(parse_form_input_by_name_quiet 'id_user' <<< "$FORM")
 
-    USER_ID=$(echo "$FORM" | parse_form_input_by_name_quiet 'id_user')
-    SITES_ALL=$(echo "$FORM" | parse_all_attr checkbox value) || return
+    # Prepare lists of hosts to mirror to
+    HOSTS_NAMES=$(parse_all_attr 'checkbox' 'hostname' <<< "$FORM") || return
+    HOSTS_NUMS=$(parse_all_attr 'checkbox' 'value' <<< "$FORM") || return
 
-    # Code copied from mirrorcreator module
-    if [ -z "$SITES_ALL" ]; then
+    if [ -z "$HOSTS_NAMES" ]; then
         log_error 'Empty list, site updated?'
         return $ERR_FATAL
     fi
 
-    log_debug "Available sites:" $SITES_ALL
+    log_debug 'Available hosts:'
+    while read -r NAME && read -r NUM <&3; do
+        log_debug "  $NAME - $NUM"
+    done <<< "$HOSTS_NAMES" 3<<< "$HOSTS_NUMS"
 
     if [ -n "$COUNT" ]; then
-        #if (( COUNT > 10 )); then
-        #    COUNT=10
-        #    log_error "Too big integer value for --count, set it to $COUNT"
-        #fi
-
-        for SITE in $SITES_ALL; do
+        for NUM in $HOSTS_NUMS; do
             (( COUNT-- > 0 )) || break
-            SITES_SEL="$SITES_SEL $SITE"
+            HOSTS_SEL="$HOSTS_SEL $NUM"
         done
     elif [ "${#INCLUDE[@]}" -gt 0 ]; then
-        for SITE in "${INCLUDE[@]}"; do
-            # FIXME: Should match word boundary (\< & \> are GNU grep extensions)
-            if match "$SITE" "$SITES_ALL"; then
-                SITES_SEL="$SITES_SEL $SITE"
+        for HOST in "${INCLUDE[@]}"; do
+            HOST_FOUND='false'
+
+            while read -r NAME && read -r NUM <&3; do
+                if [ "$HOST" == "$NAME" ]; then
+                    HOSTS_SEL="$HOSTS_SEL $NUM"
+                    HOST_FOUND='true'
+                    break
+                fi
+            done <<< "$HOSTS_NAMES" 3<<< "$HOSTS_NUMS"
+
+            if [ "$HOST_FOUND" == 'true' ]; then
+                log_debug "Added to the host list: $HOST"
             else
-                log_error "Host not supported: $SITE, ignoring"
+                log_error "Host not supported and ignored: $HOST"
             fi
         done
     else
         # Default hosting sites selection
-        SITES_SEL=$(echo "$FORM" | parse_all_attr checked value) || return
+        HOSTS_SEL=$(parse_all_attr 'checked' 'value' <<< "$FORM") || return
     fi
 
-    if [ -z "$SITES_SEL" ]; then
-        log_debug 'Empty site selection. Nowhere to upload!'
+    if [ -z "$HOSTS_SEL" ]; then
+        log_debug 'Empty host selection. Nowhere to upload!'
         return $ERR_FATAL
     fi
-    # End of code copy
 
-    # Prepare lists of hosts to mirror to
-    for HOST in $SITES_SEL; do
-        log_debug "selected site: $HOST"
-        SITES_FORM="$SITES_FORM -d box%5B%5D=$HOST"
-        SITES_MULTI="$SITES_MULTI -F box[]=$HOST"
+    # Convert prepared lists of hosts to form
+    for NUM in $HOSTS_SEL; do
+        log_debug "Selected host number: $NUM"
+        FORM_HOSTS="$FORM_HOSTS -F box[]=$NUM"
     done
 
-    # Proceed with upload
-    if match_remote_url "$FILE"; then
-        if [ "$DESTFILE" != 'dummy' ]; then
-            log_error 'Remote filename ignored, not supported by site'
-        fi
+    local UPLOAD_ID_RND SERVER FORM_NAMES FORM_VALUES FILE_NAME
+    local FILE_NAME_ORIG FORM_POST FILE_ID JSON STATUS PARAMS
 
-        UPLOAD_ID=$(echo "$FORM" | \
-            parse_form_input_by_id 'progress_key') || return
+    # Upload local file
+    if ! match_remote_url "$FILE"; then
+        UPLOAD_ID_RND=$(random dec 12) || return
+        SERVER=$(parse_form_input_by_name 'server' <<< "$FORM") || return
 
-        PAGE=$(curl_with_log -b "$COOKIE_FILE" \
-            -F "APC_UPLOAD_PROGRESS=$UPLOAD_ID" \
+        PAGE=$(curl_with_log \
+            -F "uploadID=$UPLOAD_ID" \
             -F "id_user=$USER_ID" \
-            -F "url=$FILE" \
-            $SITES_MULTI \
-            "$UPLOAD_BASE_URL/copy_remote.php") || return
+            -F "server=$SERVER" \
+            -F "file_0=@$FILE;filename=$DESTFILE" \
+            -F 'file_1=;filename=' \
+            $FORM_HOSTS \
+            "$UPLOAD_URL?upload_id=$UPLOAD_ID_RND&js_on=1&xpass=&xmode=1" \
+            | break_html_lines) || return
 
-        if ! match 'Your link' "$PAGE"; then
-            log_error 'Error uploading to server'
-            return $ERR_FATAL
-        fi
-        LINK="http://www.go4up.com/dl/$UPLOAD_ID"
+        FORM_NAMES=$(parse_all_attr 'textarea' 'name' <<< "$PAGE") || return
+        FORM_VALUES=$(parse_all_tag_quiet 'textarea' <<< "$PAGE")
 
-    else
-        local UPLOAD_URL1 UPLOAD_URL2
-
-        # Site uses UberUpload for direct upload
-        UPLOAD_URL1=$(echo "$PAGE" | \
-            parse 'path_to_link_script' '"\([^"]\+\)"') || return
-        UPLOAD_URL2=$(echo "$PAGE" | \
-            parse 'path_to_upload_script' '"\([^"]\+\)"') || return
+        # Note: Form must be send in multipart manner, but in a filename
+        #       we may have spaces, so individually treat a filename in
+        #       curl command and quote it there.
+        while read -r NAME && read -r VALUE <&3; do
+            if [ "$NAME" == 'file_name[]' ]; then
+                FILE_NAME="$VALUE"
+            elif [ "$NAME" == 'file_name_orig[]' ]; then
+                FILE_NAME_ORIG="$VALUE"
+            else
+                FORM_POST="$FORM_POST -F $NAME=$(uri_encode <<< $VALUE)"
+            fi
+        done <<< "$FORM_NAMES" 3<<< "$FORM_VALUES"
 
         PAGE=$(curl -b "$COOKIE_FILE" \
-            $SITES_FORM \
-            -d "id_user=$USER_ID" \
-            -d "upload_file[]=$DESTFILE" \
-            "$UPLOAD_BASE_URL$UPLOAD_URL1") || return
+            -F "file_name[]=$FILE_NAME" \
+            -F "file_name_orig[]=$FILE_NAME_ORIG" \
+            $FORM_POST \
+            -L "$BASE_URL/home/upload_process") || return
 
-        # if(typeof UberUpload.startUpload == 'function')
-        # { UberUpload.startUpload("f7c3511c7eac0716dc64bba7e32ef063",0,0); }
-        UPLOAD_ID=$(echo "$PAGE" | parse 'startUpload(' '"\([^"]\+\)"') || return
-        log_debug "Upload ID: $UPLOAD_ID"
+    # Upload remote file
+    else
+        FILE_ID=$(parse_form_input_by_name 'FILEID' <<< "$FORM") || return
 
-        # Note: No need to call ubr_set_progress.php, ubr_get_progress.php
-
-        PAGE=$(curl_with_log -b "$COOKIE_FILE" \
+        # Note: We cannot force curl to send a POST and not wait for a response,
+        #       so asynchronous uploads are not possible.
+        JSON=$(curl \
+            -F "uploadID=$UPLOAD_ID" \
+            -F "FILEID=$FILE_ID" \
             -F "id_user=$USER_ID" \
-            -F "upfile_$(date +%s)000=@$FILE;filename=$DESTFILE" \
-            $SITES_MULTI \
-            "$UPLOAD_BASE_URL$UPLOAD_URL2?upload_id=$UPLOAD_ID") || return
+            -F "url[]=$FILE" \
+            -F 'rename[]=' \
+            $FORM_HOSTS \
+            "$UPLOAD_URL/upload/remote") || return
 
-        # parent.UberUpload.redirectAfterUpload('../../uploaded.php?upload_id=9f07...
-        UPLOAD_URL1=$(echo "$PAGE" | \
-            parse 'redirectAfter' "('\([^']\+\)'") || return
+        STATUS=$(parse_json 'status' <<< "$JSON") || return
+        if [ "$STATUS" != '1' ]; then
+            ERR=$(parse_json_quiet 'msg' <<< "$JSON")
+            log_error "Remote error: $ERR"
+            return $ERR_FATAL
+        fi
 
-        PAGE=$(curl -b "$COOKIE_FILE" "$UPLOAD_URL1") || return
+        PARAMS=$(parse_json 'params' <<< "$JSON") || return
 
-        LINK=$(echo "$PAGE" | parse_attr '/dl/' href) || return
+        PAGE=$(curl -b "$COOKIE_FILE" \
+            -F 'params='$PARAMS'' \
+            -L "$BASE_URL/home/upload_process/remote") || return
     fi
 
-    echo "$LINK"
+    parse_attr '/dl/' 'href' <<< "$PAGE" || return
 }
 
 # List links from a go4up link
@@ -263,7 +292,7 @@ go4up_upload() {
 go4up_list() {
     local URL=$1
     local BASE_URL='http://go4up.com'
-    local PAGE INFO_URL LINKS NAME
+    local PAGE INFO_URL LINKS FILE_NAME
 
     PAGE=$(curl -L "$URL") || return
 
@@ -275,17 +304,18 @@ go4up_list() {
 
     # url: "/download/gethosts/<ID>/<Name>",
     INFO_URL=$(parse '^[[:space:]]*url:' '"\(.\+\)"' <<< "$PAGE") || return
-    NAME=$(parse '<h3' '<h3[[:space:]]*>\(.\+\)[[:space:]]\+([[:alnum:]]\+' <<< "$PAGE") || return
+    FILE_NAME=$(parse '<h3' '<h3[[:space:]]*>\(.\+\)[[:space:]]\+([[:alnum:]]\+' \
+        <<< "$PAGE") || return
 
-    PAGE=$(curl "$BASE_URL$INFO_URL" | break_html_lines) || return
-    LINKS=$(parse_all_attr 'href' <<< "$PAGE") || return
+    PAGE=$(curl "$BASE_URL$INFO_URL" | break_html_lines_alt) || return
+    PAGE=$(replace_all '\"' '"' <<< "$PAGE")
+    PAGE=$(replace_all '\/' '/' <<< "$PAGE")
+    LINKS=$(parse_all_attr '^<a' 'href' <<< "$PAGE") || return
 
     while read INFO_URL; do
-        [[ "$INFO_URL" = http://* ]] && continue
-
         PAGE=$(curl "$BASE_URL$INFO_URL") || return
         parse_attr '<b>' 'href' <<< "$PAGE" || return
-        echo "$NAME"
+        echo "$FILE_NAME"
     done <<< "$LINKS"
 }
 
@@ -296,7 +326,7 @@ go4up_delete() {
     local COOKIE_FILE=$1
     local URL=$2
     local BASE_URL='http://go4up.com'
-    local PAGE FILE_ID
+    local PAGE FILE_ID FILE_NAME
 
     test "$AUTH_FREE" || return $ERR_LINK_NEED_PERMISSIONS
 
@@ -306,23 +336,33 @@ go4up_delete() {
     FILE_ID=$(echo "$URL" | parse . '[=/]\([[:alnum:]]\+\)$') || return
     log_debug "File ID: $FILE_ID"
 
+    go4up_switch_lang "$COOKIE_FILE" "$BASE_URL"
+
     # Check link
-    PAGE=$(curl "$URL") || return
-    match 'does not exist or has been removed' "$PAGE" && \
+    PAGE=$(curl -b "$COOKIE_FILE" "$URL") || return
+    if match 'File not Found' "$PAGE"; then
         return $ERR_LINK_DEAD
+    fi
+
+    FILE_NAME=$(parse '<h3' '<h3[[:space:]]*>\(.\+\)[[:space:]]\+([[:alnum:]]\+' \
+        <<< "$PAGE") || return
 
     go4up_login "$AUTH_FREE" "$COOKIE_FILE" "$BASE_URL" || return
-    PAGE=$(curl -b "$COOKIE_FILE" -d "id=$FILE_ID" \
-        "$BASE_URL/delete.php") || return
 
-    # Note: Go4up will *always* send this reply
-    match 'Your link has been deleted from our database' "$PAGE" || \
+    PAGE=$(curl -b "$COOKIE_FILE" -F "FILEID=$FILE_ID" -F "filename=$FILE_NAME" \
+        "$BASE_URL/manager/filemanager/delete") || return
+
+    # Note: Go4up will *always* send this reply, 1 - success, 2 - failed.
+    #       Nevertheless it also return success for anonymous files,
+    #       so we have to check if the link is really gone.
+    if ! match '1' "$PAGE"; then
         return $ERR_FATAL
+    fi
 
     # Check if link is really gone
-    PAGE=$(curl "$URL") || return
-    if ! match 'does not exist or has been removed' "$PAGE"; then
-        log_error 'File NOT removed. Correct account?'
+    PAGE=$(curl -b "$COOKIE_FILE" "$URL") || return
+    if ! match 'File not Found' "$PAGE"; then
+        log_error 'File NOT removed. Are the owner if this file?'
         return $ERR_LINK_NEED_PERMISSIONS
     fi
 }
