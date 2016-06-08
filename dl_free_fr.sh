c840a1c0801197fd06248f41eb26b18082319162
Raziel-23 <venom23@runbox.com>
2016-06-08 21:22:36 +0200

    dl_free_fr: upstream update
    Additionally:
    - implements support for password protected files
    - change 'echo' to '<<<'
    
    Fixes #91.

--- dl_free_fr.sh
+++ dl_free_fr.sh
@@ -1,5 +1,5 @@
 # Plowshare dl.free.fr module
-# Copyright (c) 2010-2013 Plowshare team
+# Copyright (c) 2010-2016 Plowshare team
 #
 # This file is part of Plowshare.
 #
@@ -18,7 +18,8 @@
 
 MODULE_DL_FREE_FR_REGEXP_URL='http://dl\.free\.fr/'
 
-MODULE_DL_FREE_FR_DOWNLOAD_OPTIONS=""
+MODULE_DL_FREE_FR_DOWNLOAD_OPTIONS="
+LINK_PASSWORD,p,link-password,S=PASSWORD,Used in password-protected files"
 MODULE_DL_FREE_FR_DOWNLOAD_RESUME=yes
 MODULE_DL_FREE_FR_DOWNLOAD_FINAL_LINK_NEEDS_COOKIE=yes
 MODULE_DL_FREE_FR_DOWNLOAD_FINAL_LINK_NEEDS_EXTRA=()
@@ -35,87 +36,6 @@ MODULE_DL_FREE_FR_DELETE_OPTIONS=""
 
 MODULE_DL_FREE_FR_PROBE_OPTIONS=""
 
-declare -r AYL_SERVER='http://api.adyoulike.com/'
-# Adyoulike decoding function
-# Main engine: http://api-ayl.appspot.com/static/js/ayl_lib.js
-#
-# $1: AYL site public key
-# $2 (optional): env value (default is "prod")
-# stdout: On 4 lines: <word> \n <challenge> \n <token_id> \n <transaction_id>
-captcha_ayl_process() {
-    local ENV=${2:-prod}
-    local URL="${AYL_SERVER}challenge?key=${1}&env=$ENV"
-    local TRY VARS TYPE WORDS RESPONSE TOKEN TOKEN_ID TID FILENAME
-
-    TRY=0
-    # Arbitrary 100 limit is safer
-    while (( TRY++ < 100 )) || return $ERR_MAX_TRIES_REACHED; do
-        log_debug "Adyoulike loop $TRY"
-
-        VARS=$(curl -L "$URL") || return
-
-        if [ -z "$VARS" ]; then
-            return $ERR_CAPTCHA
-        fi
-
-        WORDS=$(echo "$VARS" | parse_json_quiet 'instructions_visual' | tr -d '\302')
-        TOKEN=$(echo "$VARS" | parse_json 'token') || return
-        TOKEN_ID=$(echo "$VARS" | parse_json 'tid') || return
-        TYPE=$(echo "$VARS" | parse_json 'medium_type') || return
-
-        log_debug "Adyoulike challenge: '$TOKEN'"
-        log_debug "Adyoulike type: '$TYPE'"
-
-        # Easy case, captcha answer is written plain text :)
-        # UTF-8 characters: « (\uC2AB), » (\uC2BB)
-        if [ $TYPE = 'image/adyoulike' ]; then
-            if [ -z "$WORDS" ]; then
-                log_error "$FUNCNAME: instructions_visual empty, skipping"
-            fi
-
-            RESPONSE=$(parse_quiet . '"\([^"]\+\)"' <<< "$WORDS")
-            if [ -z "$RESPONSE" ]; then
-                # FIXME: Don't use \xHH in basic POSIX regexp
-                RESPONSE=$(parse_quiet . '\xAB \([^[:space:]]*\) \xBB' <<< "$WORDS")
-                if [ -z "$RESPONSE" ]; then
-                    matchi 'Cliquez sur la publicit. puis validez' "$WORDS" && \
-                        RESPONSE='[passthroughtoken]'
-                fi
-            fi
-
-            [ -n "$RESPONSE" ] && break
-
-            log_debug "Adyoulike instr: '$WORDS'"
-
-        #elif [ "$TYPE" = 'video/youtube' ];
-        else
-            log_error "$FUNCNAME: $TYPE not handled, skipping"
-        fi
-
-        # Maybe we'll need this later?
-        # curl -b "ayl_tid=$TOKEN_ID" "${AYL_SERVER}iframe?iframe_type=${TYPE//\//_}&token=${TOKEN}&env=$ENV"
-
-        FILENAME=$(create_tempfile '.ayl.jpg') || return
-        curl -b "ayl_tid=$TOKEN_ID" -o "$FILENAME" \
-            "${AYL_SERVER}resource?token=${TOKEN}&env=$ENV" || return
-
-        WORDS=$(captcha_process "$FILENAME" ayl) || return
-        rm -f "$FILENAME"
-
-        { read RESPONSE; read TID; } <<<"$WORDS"
-
-        [ -n "$RESPONSE" ] && break
-
-        # Reload image
-        log_debug 'empty, request another image'
-    done
-
-    echo "$RESPONSE"
-    echo "$TOKEN"
-    echo "$TOKEN_ID"
-    echo ${TID:-0}
-}
-
 # Output a dl.free.fr file download URL (anonymous)
 # $1: cookie file
 # $2: dl.free.fr url
@@ -123,18 +43,12 @@ captcha_ayl_process() {
 dl_free_fr_download() {
     local -r COOKIE_FILE=$1
     local URL=$2
-    local PAGE FORM_HTML FORM_ACTION FORM_FILE FORM_SUBM SESSID FILE_NAME
+    local PAGE FORM_HTML FORM_ACTION FORM_FILE SESSID FILE_NAME
 
     # Notes:
     # - "curl -I" (HTTP HEAD request) is ignored (returns 404 error)
     # - Range request is ignored for non Free ISP users (due to redir?)
     PAGE=$(curl -L -i -r 0-1023 "$URL") || return
-
-    # WWW-Authenticate: Basic realm="Autorisation requise"
-    if match '^HTTP/1.1 401' "$PAGE"; then
-        #PAGE=$(curl --basic --user ":password" -L -i -r 0-1024 "$URL") || return
-        return $ERR_LINK_NEED_PERMISSIONS
-    fi
 
     # Bad link
     if match '^HTTP/1.1 404' "$PAGE"; then
@@ -153,7 +67,7 @@ dl_free_fr_download() {
         MODULE_DL_FREE_FR_DOWNLOAD_FINAL_LINK_NEEDS_COOKIE=no
         MODULE_DL_FREE_FR_DOWNLOAD_FINAL_LINK_NEEDS_EXTRA=(--retry 2)
 
-        FILE_NAME=$(echo "$PAGE" | grep_http_header_content_disposition) || return
+        FILE_NAME=$(grep_http_header_content_disposition <<< "$PAGE") || return
 
         echo "$URL"
         echo "$FILE_NAME"
@@ -168,39 +82,39 @@ dl_free_fr_download() {
         return $ERR_LINK_DEAD
     fi
 
-    FILE_NAME=$(echo "$PAGE" | parse 'Fichier:' '">\([^<]*\)' 1) || return
+    FILE_NAME=$(parse '>Fichier<' '>\([^<>]*\) <br' <<< "$PAGE") || return
 
-    FORM_HTML=$(grep_form_by_order "$PAGE" 2) || return
-    FORM_ACTION=$(echo "$FORM_HTML" | parse_form_action) || return
-    FORM_FILE=$(echo "$FORM_HTML" | parse_form_input_by_name 'file' | uri_encode_strict)
-    FORM_SUBM=$(echo "$FORM_HTML" | parse_form_input_by_type 'submit' | uri_encode_strict)
+    FORM_HTML=$(grep_form_by_order "$PAGE" 1) || return
+    FORM_ACTION=$(parse_form_action <<< "$FORM_HTML") || return
+    FORM_FILE=$(parse_form_input_by_name 'file' <<< "$FORM_HTML" | uri_encode_strict) || return
 
-    local PUBKEY WTTI WORD TOKEN TKID ID
-    PUBKEY='P~zQ~O0zV0WTiAzC-iw0navWQpCLoYEP'
-    WTTI=$(captcha_ayl_process $PUBKEY) || return
-    { read WORD; read TOKEN; read TKID; read ID; } <<<"$WTTI"
-
-    PAGE=$(curl -c "$COOKIE_FILE" \
+    # Here we set a cookie file for a final link.
+    # Notes: "curl -I" (HTTP HEAD request) is ignored.
+    PAGE=$(curl -L -i -r 0-1023 -c "$COOKIE_FILE" \
         -d "file=$FORM_FILE" \
-        -d "submit=$FORM_SUBM" \
-        -d "_ayl_response=$WORD" \
-        -d "_ayl_token_challenge=$TOKEN" \
-        -d '_ayl_captcha_engine=adyoulike' \
-        -d '_ayl_utf8_ie_fix=%E2%98%83' \
-        -d "_ayl_tid=$TKID" \
-        -d '_ayl_env=prod' \
         --referer "$URL" \
         "http://dl.free.fr/$FORM_ACTION") || return
 
-    # Could also check for "Code incorrect" in $PAGE
-    SESSID=$(parse_cookie_quiet 'getfile' < "$COOKIE_FILE")
-    if [ -z "$SESSID" ]; then
-        captcha_nack $ID
-        return $ERR_CAPTCHA
-    fi
+    # WWW-Authenticate: Basic realm="Autorisation requise"
+    if match '^HTTP/1.1 401' "$PAGE"; then
+        log_debug 'File is password protected.'
 
-    captcha_ack $ID
-    log_debug 'correct captcha'
+        if [ -z "$LINK_PASSWORD" ]; then
+            LINK_PASSWORD=$(prompt_for_password) || return
+        fi
+
+        # Notes: "curl -I" (HTTP HEAD request) is ignored (returns 404 error)
+        PAGE=$(curl -L -i -r 0-1023 -b "$COOKIE_FILE" \
+            --user ":$LINK_PASSWORD" \
+            "$URL") || return
+
+        if match '^HTTP/1.1 401' "$PAGE"; then
+            log_error 'Wrong password'
+            return $ERR_LINK_PASSWORD_REQUIRED
+        fi
+
+        MODULE_DL_FREE_FR_DOWNLOAD_FINAL_LINK_NEEDS_EXTRA=(--user ":$LINK_PASSWORD")
+    fi
 
     echo "$URL"
     echo "$FILE_NAME"
@@ -220,7 +134,7 @@ dl_free_fr_upload() {
     PAGE=$(curl "$UPLOADURL") || return
 
     FORM_HTML=$(grep_form_by_order "$PAGE" 2) || return
-    FORM_ACTION=$(echo "$FORM_HTML" | parse_form_action) || return
+    FORM_ACTION=$(parse_form_action <<< "$FORM_HTML") || return
 
     # <input> markers are: ufile, mail1, mail2, mail3, mail4, message, password
     # Returns 302. Answer headers are not returned with -i switch, I must
@@ -266,10 +180,10 @@ dl_free_fr_upload() {
             break
         # Fichier "foo" en ligne, procédure terminée avec succès...
         elif match 'Le fichier sera accessible' "$PAGE"; then
-            DL_URL=$(echo "$PAGE" | parse 'en ligne' \
-                "window\.open('\(http://dl.free.fr/[^?]*\)')" | html_to_utf8)
-            DEL_URL=$(echo "$PAGE" | parse 'en ligne' \
-                "window\.open('\(http://dl.free.fr/rm\.pl[^']*\)" | html_to_utf8)
+            DL_URL=$(parse 'en ligne' "window\.open('\(http://dl.free.fr/[^?]*\)')" \
+                <<< "$PAGE" | html_to_utf8) || return
+            DEL_URL=$(parse 'en ligne' "window\.open('\(http://dl.free.fr/rm\.pl[^']*\)" \
+                <<< "$PAGE" | html_to_utf8) || return
 
             echo "$DL_URL"
             echo "$DEL_URL"
@@ -300,7 +214,7 @@ dl_free_fr_delete() {
 
     # Si vous souhaitez réelement supprimer le fichier nommé [<FILE_NAME>] cliquez ici
     if match 'Si vous souhaitez r&eacute;element supprimer' "$PAGE"; then
-        URL=$(echo "$PAGE" | parse_attr 'Si vous souhaitez' 'href') || return
+        URL=$(parse_attr 'Si vous souhaitez' 'href' <<< "$PAGE") || return
         PAGE=$(curl "$BASE_URL$URL") || return
 
         # Le fichier nommé [<FILE_NAME>] a été supprimé avec succès.
@@ -332,11 +246,11 @@ dl_free_fr_probe() {
     # Free is your ISP, this is direct download
     if match '^HTTP/1.1 206' "$PAGE"; then
         if [[ $REQ_IN = *f* ]]; then
-            FILE_NAME=$(echo "$PAGE" | grep_http_header_content_disposition) &&
+            FILE_NAME=$(grep_http_header_content_disposition <<< "$PAGE") &&
                 echo "$FILE_NAME" && REQ_OUT="${REQ_OUT}f"
         fi
         if [[ $REQ_IN = *s* ]]; then
-            FILE_SIZE=$(echo "$PAGE" | parse '^Content-Range:' '/\([[:digit:]]*\)') && \
+            FILE_SIZE=$(parse '^Content-Range:' '/\([[:digit:]]*\)' <<< "$PAGE") && \
                 echo "$FILE_SIZE" && REQ_OUT="${REQ_OUT}s"
         fi
 
@@ -345,11 +259,11 @@ dl_free_fr_probe() {
         matchi 'erreur[[:space:]][45]' "$PAGE" && return $ERR_LINK_DEAD
 
         if [[ $REQ_IN = *f* ]]; then
-            FILE_NAME=$(echo "$PAGE" | parse 'Fichier:' '">\([^<]*\)' 1) &&
+            FILE_NAME=$(parse '>Fichier<' '>\([^<>]*\) <br' <<< "$PAGE") &&
                 echo "$FILE_NAME" && REQ_OUT="${REQ_OUT}f"
         fi
         if [[ $REQ_IN = *s* ]]; then
-            FILE_SIZE=$(echo "$PAGE" | parse 'Taille:' '">\([^s]*\)' 1) &&
+            FILE_SIZE=$(parse '>Taille<' '>\([^<>]*\) <br' <<< "$PAGE") &&
                 translate_size "${FILE_SIZE/o/B}" && REQ_OUT="${REQ_OUT}s"
         fi
     fi
