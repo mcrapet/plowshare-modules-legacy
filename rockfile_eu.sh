78f99335c166ebd7205c04c2c0369808bc6d6313
Raziel-23 <venom23@runbox.com>
2016-06-19 18:36:47 +0200

    rockfile_eu (upload): add much more options
    Adds:
    - checking file size depending on an account type
    - support for remote links
    - choosing folders with subfolders
    - setting a premium flag for a file
    - setting a publish flag for a file
    - setting a proxy for remote links

--- rockfile_eu.sh
+++ rockfile_eu.sh
@@ -20,10 +20,14 @@ MODULE_ROCKFILE_EU_REGEXP_URL='https\?://\(www\.\)\?rockfile\.eu/'
 
 MODULE_ROCKFILE_EU_UPLOAD_OPTIONS="
 AUTH,a,auth,a=USER:PASSWORD,User account (mandatory)
-DESCRIPTION,d,description,S=DESCRIPTION,Set file description
 LINK_PASSWORD,p,link-password,S=PASSWORD,Protect a link with a password
-TOEMAIL,,email-to,e=EMAIL,<To> field for notification email"
-MODULE_ROCKFILE_EU_UPLOAD_REMOTE_SUPPORT=no
+FOLDER,,folder,s=FOLDER,Folder to upload files into (support subfolders)
+DESCRIPTION,d,description,S=DESCRIPTION,Set file description
+TOEMAIL,,email-to,e=EMAIL,<To> field for notification email
+PREMIUM_FILE,,premium,,Make file inaccessible to non-premium users
+PUBLISH_FILE,,publish,,Mark file to be published
+PROXY,,proxy,s=PROXY,Proxy for a remote link"
+MODULE_ROCKFILE_EU_UPLOAD_REMOTE_SUPPORT=yes
 
 # Static function. Check for and handle "DDoS protection"
 # $1: full content of initial page
@@ -182,6 +186,67 @@ rockfile_eu_login() {
     echo $TYPE
 }
 
+# Static function. Check if specified folder name is valid.
+# If folder not found then create it. Support subfolders.
+# $1: folder name selected by user
+# $2: cookie file (logged into account)
+# $3: base URL
+# stdout: folder id
+rockfile_eu_check_folder() {
+    local -r NAME=$1
+    local -r COOKIE_FILE=$2
+    local -r BASE_URL=$3
+    local -r API_URL=$4
+    local -r API_DATA=$5
+    local FOLDER_NAMES FOLDER PAGE FOLDER_ID
+    local FORM_HTML FORM_OP FORM_TOKEN FORM_FLD_ID FORM_KEY
+
+    # The following characters cannot be used with parse.
+    if match '["\\\[\]<>]' "$NAME"; then
+        log_error 'Folder name should not contain the following characters: "\\\[\]<>'
+        return $ERR_FATAL
+    fi
+
+    # Convert subfolders names into an array.
+    IFS='/' read -ra FOLDER_NAMES <<< "$NAME"
+
+    FOLDER_ID=0
+
+    for FOLDER in "${FOLDER_NAMES[@]}"; do
+        # Skip empty names.
+        [ -z "$FOLDER" ] && continue
+
+        PAGE=$(curl -b "$COOKIE_FILE" "$BASE_URL/?op=my_files&fld_id=$FOLDER_ID") || return
+        FOLDER_ID=$(parse_quiet . 'fld_id=\([^"]\+\)".*>'"$FOLDER"'<' <<< "$PAGE")
+
+        # Create new folder.
+        if [ -z "$FOLDER_ID" ]; then
+            FORM_HTML=$(grep_form_by_name "$PAGE" 'F1') || return
+            FORM_OP=$(parse_form_input_by_name 'op' <<< "$FORM_HTML") || return
+            FORM_TOKEN=$(parse_form_input_by_name 'token' <<< "$FORM_HTML") || return
+            FORM_FLD_ID=$(parse_form_input_by_name 'fld_id' <<< "$FORM_HTML") || return
+            FORM_KEY=$(parse_form_input_by_name_quiet 'key' <<< "$FORM_HTML")
+
+            PAGE=$(curl -b "$COOKIE_FILE" \
+                -d "op=$FORM_OP" \
+                -d "token=$FORM_TOKEN" \
+                -d "fld_id=$FORM_FLD_ID" \
+                -d "key=$FORM_KEY" \
+                -d "create_new_folder=$FOLDER" \
+                -d 'to_folder=' \
+                -L "$BASE_URL") || return
+
+            FOLDER_ID=$(parse . 'fld_id=\([^"]\+\)".*>'"$FOLDER"'<' <<< "$PAGE") || return
+            log_debug "Successfully created: '$FOLDER' with ID '$FOLDER_ID'"
+        else
+            log_debug "Successfully found: '$FOLDER' with ID '$FOLDER_ID'"
+        fi
+    done
+
+    log_debug "FOLDER ID: '$FOLDER_ID'"
+    echo $FOLDER_ID
+}
+
 # Upload a file to rockfile.eu
 # $1: cookie file
 # $2: input file (with full path)
@@ -192,59 +257,164 @@ rockfile_eu_upload() {
     local -r FILE=$2
     local -r DESTFILE=$3
     local -r BASE_URL='https://rockfile.eu'
+    local ACCOUNT MAX_SIZE SIZE FOLDER_ID PAGE USER_TYPE UPLOAD_ID
 
-    local ACCOUNT PAGE USER_TYPE UPLOAD_ID TAGS_STR
-    local FORM_HTML FORM_ACTION FORM_UTYPE FORM_SESS FORM_SRV_TMP FORM_BUTTON
-    local FORM_FN FORM_ST FORM_OP
-
+    # User account is mandatory
     if [ -n "$AUTH" ]; then
         ACCOUNT=$(rockfile_eu_login "$AUTH" "$COOKIE_FILE" "$BASE_URL") || return
     else
         return $ERR_LINK_NEED_PERMISSIONS
     fi
 
+    # Sanity checks
+    if match_remote_url "$FILE"; then
+        if [ -n "$DESCRIPTION" ]; then
+            log_error 'You cannot set description for remote link.'
+            return $ERR_LINK_NEED_PERMISSIONS
+        fi
+    else
+        if [ -n "$PROXY" ]; then
+            log_error 'You can use proxy only with remote link.'
+            return $ERR_LINK_NEED_PERMISSIONS
+        fi
+    fi
+
+    # File size check
+    if ! match_remote_url "$FILE"; then
+        # Note: Max upload file size for 'free' users is limited to 2 GiB,
+        #       for 'premium' accounts is limited to 6 GiB.
+        if [ "$ACCOUNT" = 'free' ]; then
+            MAX_SIZE=2147483648 # 2 GiB
+        else
+            MAX_SIZE=6442450944 # 6 MiB
+        fi
+
+        SIZE=$(get_filesize "$FILE")
+        if [ $SIZE -gt $MAX_SIZE ]; then
+            log_debug "File is bigger than $MAX_SIZE for $ACCOUNT user."
+            return $ERR_SIZE_LIMIT_EXCEEDED
+        fi
+    fi
+
+    # Choose a folder
+    if [ -n "$FOLDER" ]; then
+        FOLDER_ID=$(rockfile_eu_check_folder "$FOLDER" "$COOKIE_FILE" \
+            "$BASE_URL") || return
+    fi
+
     PAGE=$(curl -b "$COOKIE_FILE" "$BASE_URL/upload_files") || return
-    FORM_HTML=$(grep_form_by_name "$PAGE" 'file') || return
-    FORM_ACTION=$(parse_form_action <<< "$PAGE") || return
-    FORM_UTYPE=$(parse_form_input_by_name 'upload_type' <<< "$PAGE") || return
-    FORM_SESS=$(parse_form_input_by_name_quiet 'sess_id' <<< "$PAGE")
-    FORM_SRV_TMP=$(parse_form_input_by_name 'srv_tmp_url' <<< "$PAGE") || return
-    FORM_BUTTON=$(parse_form_input_by_name 'submit_btn' <<< "$PAGE") || return
 
     # "reg"
     USER_TYPE=$(parse 'var utype' "='\([^']*\)" <<< "$PAGE") || return
     log_debug "User type: '$USER_TYPE'"
 
     UPLOAD_ID=$(random dec 12) || return
-    PAGE=$(curl_with_log \
-        -F "upload_type=$FORM_UTYPE" \
-        -F "sess_id=$FORM_SESS" \
-        -F "srv_tmp_url=$FORM_TMP_SRV" \
-        -F "file_0=@$FILE;filename=$DESTFILE" \
-        --form-string "file_0_descr=$DESCRIPTION" \
-        --form-string "link_rcpt=$TOEMAIL" \
-        --form-string "link_pass=$LINK_PASSWORD" \
-        --form-string 'to_folder=' \
-        --form-string "submit_btn=$FORM_BUTTON" \
-        "${FORM_ACTION}${UPLOAD_ID}&utype=${USER_TYPE}&js_on=1&upload_type=${FORM_UTYPE}" | break_html_lines) || return
 
+    local FORM_HTML FORM_ACTION FORM_SESS FORM_UTYPE FORM_SRV_TMP FORM_BUTTON FORM_TOS
+    local FORM_FN FORM_ST FORM_OP TOEMAIL_DATA FILE_URL FILE_DEL_URL FILE_ID
+
+    # Upload local file
+    if ! match_remote_url "$FILE"; then
+        FORM_HTML=$(grep_form_by_name "$PAGE" 'file') || return
+        FORM_ACTION=$(parse_form_action <<< "$PAGE") || return
+        FORM_UTYPE=$(parse_form_input_by_name 'upload_type' <<< "$PAGE") || return
+        FORM_SESS=$(parse_form_input_by_name_quiet 'sess_id' <<< "$PAGE")
+        FORM_SRV_TMP=$(parse_form_input_by_name 'srv_tmp_url' <<< "$PAGE") || return
+        FORM_BUTTON=$(parse_form_input_by_name 'submit_btn' <<< "$PAGE") || return
+
+        PAGE=$(curl_with_log \
+            -F "upload_type=$FORM_UTYPE" \
+            -F "sess_id=$FORM_SESS" \
+            -F "srv_tmp_url=$FORM_TMP_SRV" \
+            -F 'file_0=;filename=' \
+            -F "file_0=@$FILE;filename=$DESTFILE" \
+            --form-string "file_0_descr=$DESCRIPTION" \
+            --form-string "link_rcpt=$TOEMAIL" \
+            --form-string "link_pass=$LINK_PASSWORD" \
+            -F "to_folder=$FOLDER_ID" \
+            -F 'file_1=;filename=' \
+            --form-string "submit_btn=$FORM_BUTTON" \
+            "${FORM_ACTION}${UPLOAD_ID}&utype=${USER_TYPE}&js_on=1&upload_type=${FORM_UTYPE}" \
+            | break_html_lines) || return
+
+    # Upload remote file
+    else
+        FORM_HTML=$(grep_form_by_name "$PAGE" 'url') || return
+        FORM_ACTION=$(parse_form_action <<< "$FORM_HTML") || return
+        FORM_SESS=$(parse_form_input_by_name_quiet 'sess_id' <<< "$FORM_HTML")
+        FORM_UTYPE=$(parse_form_input_by_name 'upload_type' <<< "$FORM_HTML") || return
+        FORM_SRV_TMP=$(parse_form_input_by_name 'srv_tmp_url' <<< "$FORM_HTML") || return
+        FORM_TOS=$(parse_form_input_by_name 'tos' <<< "$FORM_HTML") || return
+        FORM_BUTTON=$(parse_form_input_by_name 'submit_btn' <<< "$FORM_HTML") || return
+
+        # Note: We cannot force curl to send a POST and not wait for a response,
+        #       so asynchronous uploads are not possible.
+        PAGE=$(curl \
+            -F "sess_id=$FORM_SESS" \
+            -F "upload_type=$FORM_UTYPE" \
+            -F "srv_tmp_url=$FORM_SRV_TMP" \
+            -F "url_mass=$FILE" \
+            --form-string "url_proxy=$PROXY" \
+            --form-string "link_rcpt=$TOEMAIL" \
+            --form-string "link_pass=$LINK_PASSWORD" \
+            -F "to_folder=$FOLDER_ID" \
+            -F 'tos=1' \
+            --form-string "submit_btn=$FORM_BUTTON" \
+            "${FORM_ACTION}${UPLOAD_ID}&js_on=1&utype=${USER_TYPE}&upload_type=${FORM_UTYPE}" \
+            | break_html_lines) || return
+    fi
+
+    # Note: The following code is the same for local and remote uploads.
     FORM_ACTION=$(parse_form_action <<< "$PAGE") || return
     FORM_FN=$(parse_tag "name='fn'" textarea <<< "$PAGE") || return
     FORM_ST=$(parse_tag "name='st'" textarea <<< "$PAGE") || return
     FORM_OP=$(parse_tag "name='op'" textarea <<< "$PAGE") || return
+    [ -n "$TOEMAIL" ] && TOEMAIL_DATA="-d link_rcpt=$TOEMAIL"
 
-    if [ "$FORM_ST" = 'OK' ]; then
-        PAGE=$(curl -b "$COOKIE_FILE" \
-            -d "fn=$FORM_FN" \
-            -d "st=$FORM_ST" \
-            -d "op=$FORM_OP" \
-            "$FORM_ACTION") || return
-
-        parse '>Download Link<' '">\(.*\)$' 1 <<< "$PAGE" || return
-        parse '>Delete Link<' '">\(.*\)$' 1 <<< "$PAGE" || return
-        return 0
+    if [ "$FORM_ST" != 'OK' ]; then
+        log_error "Unexpected status: $FORM_ST"
+        return $ERR_FATAL
     fi
 
-    log_error "Unexpected status: $FORM_ST"
-    return $ERR_FATAL
+    PAGE=$(curl -b "$COOKIE_FILE" \
+        -d "fn=$FORM_FN" \
+        -d "st=$FORM_ST" \
+        -d "op=$FORM_OP" \
+        $TOEMAIL_DATA \
+        "$FORM_ACTION") || return
+
+    FILE_URL=$(parse '>Download Link<' '">\(.*\)$' 1 <<< "$PAGE") || return
+    FILE_DEL_URL=$(parse '>Delete Link<' '">\(.*\)$' 1 <<< "$PAGE") || return
+
+    # Note: Set premium and publish flag after uploading a file.
+    if [ -n "$PREMIUM_FILE" -o -n "$PUBLISH_FILE" ]; then
+
+        PAGE=$(curl -b "$COOKIE_FILE" "$BASE_URL/?op=my_files&fld_id=$FOLDER_ID") || return
+        FILE_ID=$(parse "$FILE_URL" 'value="\(.*\)"' -1 <<< "$PAGE") || return
+
+        if [ -n "$PREMIUM_FILE" ]; then
+            log_debug 'Setting premium flag...'
+
+            PAGE=$(curl -b "$COOKIE_FILE" \
+                "$BASE_URL/?op=my_files&set_flag=file_premium_only&value=true&file_id\[\]=$FILE_ID") || return
+
+            if ! match 'OK' "$PAGE"; then
+                log_error 'Could not set premium flag.'
+            fi
+        fi
+
+        if [ -n "$PUBLISH_FILE" ]; then
+            log_debug 'Setting publish flag...'
+
+            PAGE=$(curl -b "$COOKIE_FILE" \
+                "$BASE_URL/?op=my_files&set_flag=file_public&value=true&file_id\[\]=$FILE_ID") || return
+
+            if ! match 'OK' "$PAGE"; then
+                log_error 'Could not set publish flag.'
+            fi
+        fi
+    fi
+
+    echo "$FILE_URL"
+    echo "$FILE_DEL_URL"
 }
