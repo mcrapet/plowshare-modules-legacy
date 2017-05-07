34e4176b0392a0b33f3a8dd97082d393657362e8
Julien Rolland <julien@rolland.io>
2017-05-07 18:40:36 +0200

    1fichier: url file is no longer in Location header

--- 1fichier.sh
+++ 1fichier.sh
@@ -189,15 +189,12 @@ MODULE_1FICHIER_PROBE_OPTIONS=""
         return 0
     fi
 
+    # Access to download
     PAGE=$(curl --include -b "$COOKIE_FILE" -b 'LG=en' -d '' \
         --referer "$URL" "$URL") || return
 
-    # To help you discover our services, we will not limit your download speed.
-    if match 'discover\(.*\) our services\(.*\) your download speed' "$PAGE"; then
-        FILE_URL=$(parse 'class="ok btn-general btn-orange"' '<a href="\(.*\)"  style' <<< "$PAGE")
-    else
-        FILE_URL=$(grep_http_header_location_quiet <<< "$PAGE")
-    fi
+    # Click here to download the file
+    FILE_URL=$(parse 'class="ok btn-general btn-orange"' '<a href="\(.*\)"  style' <<< "$PAGE")
 
     if [ -z "$FILE_URL" ]; then
         echo 300
