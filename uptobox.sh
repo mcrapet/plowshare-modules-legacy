419920fea382ce6397a16052bf99ff58ae8991d1
coliss86 <coliss86@users.noreply.github.com>
2018-03-08 23:29:15 +0100

    Fix #191 : small ui changes

--- uptobox.sh
+++ uptobox.sh
@@ -99,7 +99,7 @@ uptobox_download() {
         PAGE=$(curl -b "$COOKIE_FILE" "$BASE_URL/?op=my_account") || return
 
         # Opposite is: 'Upgrade to premium';
-        if match 'Renew premium' "$PAGE"; then
+        if match 'Renew Premium' "$PAGE"; then
             local DIRECT_URL
             PREMIUM=1
             DIRECT_URL=$(curl -I -b "$COOKIE_FILE" "$URL" | grep_http_header_location_quiet)
