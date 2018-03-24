bb724c72672305f79ea95c95718b742ea88be41d
coliss86 <coliss86@users.noreply.github.com>
2018-03-24 18:26:41 +0100

    uptobox : always download is https

--- uptobox.sh
+++ uptobox.sh
@@ -87,7 +87,7 @@ uptobox_cloudflare() {
 # stdout: real file download link
 uptobox_download() {
     local -r COOKIE_FILE=$1
-    local -r URL=$(replace '://www.' '://' <<< "$2")
+    local -r URL=$(replace '://www.' '://' <<< "$2" | replace 'http:' 'https:')
     local -r BASE_URL='http://uptobox.com'
     local PAGE WAIT_TIME CODE PREMIUM CAPTCHA_DATA CAPTCHA_ID
     local FORM_HTML FORM_OP FORM_ID FORM_RAND FORM_METHOD FORM_DD FORM_SZ FORM_WAITINGTOKEN
